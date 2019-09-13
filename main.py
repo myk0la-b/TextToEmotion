63fc633a0ff1a15667f0696510a1ca237c9f761c
Grusha5480 <MrGraphitem@gmail.com>
2019-09-13 17:07:58 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+from pprint import pprint
+
+
 text = input("Input your text: ")
 
 emotion_images = {"sadness": "sad_img", "happiness": "happy_img"}
 emotion_classes = {
-    "sadness": ['sad', 'bad'], "happiness": ['happy', 'good']
+    "sadness": ['sad', 'bad', 'sadness'], "happiness": ['happy', 'good']
 }
 
 words = text.split(' ')
+pprint(words)
 
 for word in words:
+    found = False
     for emotion_class in emotion_classes:
-        if word in emotion_class:
+        if word in emotion_classes[emotion_class]:
             print(emotion_class)
-        else:
-            print("Incorrect!")
+            found = True
+            break
+    if not found:
+        print("Incorrect!")

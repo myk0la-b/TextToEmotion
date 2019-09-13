6623e49aecac75e7d88684dd703f7c80307734c2
Grusha5480 <MrGraphitem@gmail.com>
2019-09-13 17:02:35 +0300

    refactoring

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ text = input("Input your text: ")
 
 emotion_images = {"sadness": "sad_img", "happiness": "happy_img"}
 emotion_classes = {
-"sadness": ['sad', 'bad'], "happiness": ['happy', 'good']
+    "sadness": ['sad', 'bad'], "happiness": ['happy', 'good']
 }
 
 words = text.split(' ')
@@ -10,6 +10,6 @@ words = text.split(' ')
 for word in words:
     for emotion_class in emotion_classes:
         if word in emotion_class:
-            print (emotion_class)
+            print(emotion_class)
         else:
             print("Incorrect!")
